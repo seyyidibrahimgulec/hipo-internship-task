e03fd58e565d6449b8eaf48aa7256924abce9fb3
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-05-13 23:06:36 +0300

    Created recipes model

--- HipoProject/settings.py
+++ HipoProject/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
+    'ingredients',
+    'interactions',
 ]
 
 MIDDLEWARE = [
--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Recipe(models.Model):
+    DIFFICULTIES = (
+        ('E', 'EASY'),
+        ('M', 'MEDIUM'),
+        ('H', 'HARD'),
+    )
+
+    title = models.CharField(max_length=300)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=1, choices=DIFFICULTIES)
+    created_time = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField()
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # likes = models.ManyToManyField()
+    # rates = models.ManyToManyField()
+    # ingredients = models.ManyToManyField()
