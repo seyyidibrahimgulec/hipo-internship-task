121f4090bbc30ade89a0b0de9c607d16bed853cc
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-06-20 15:02:05 +0300

    Add ChangePasswordTestCase and MyProfileDetailTestCase

--- users/tests.py
+++ users/tests.py
@@ -126,3 +126,93 @@ class UserAuthenticationTestCase(TestCase):
         user, response = self.create_user_and_response(email=self.params['email'], password=None)
         self.assertEqual(response.status_code, 400)
 
+
+class MyProfileDetailTestCase(TestCase):
+    url = reverse('profile_detail')
+
+    def create_user(self):
+        user = UserProfile.objects.create(username='testuser', email='testuser@mail.com')
+        user.set_password('testuser_1')
+        user.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return user, token
+
+    def test_user_can_detail(self):
+        user, token = self.create_user()
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.get(self.url, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['username'], user.username)
+        self.assertEqual(response.data['email'], user.email)
+
+    def test_incorrect_token_can_detail(self):
+        user, token = self.create_user()
+        token.key += 'ABC'
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.get(self.url, format='json')
+        self.assertEqual(response.status_code, 401)
+
+
+class ChangePasswordTestCase(TestCase):
+    url = reverse('change_password')
+
+    def create_user(self, password):
+        user = UserProfile.objects.create(username='testuser', email='testuser@mail.com')
+        user.set_password(password)
+        user.save()
+        token, created = Token.objects.get_or_create(user=user)
+        return user, token
+
+    def test_user_can_change_password(self):
+        old_password = 'testuser_1'
+        new_password = 'testuser_2'
+        user, token = self.create_user(old_password)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.put(
+            self.url,
+            {'old_password': old_password, 'new_password': new_password},
+            format='json'
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(response.data['token'], token.key)
+
+    def test_incorrect_password_can_change_password(self):
+        old_password = 'testuser_1'
+        new_password = 'testuser_2'
+        user, token = self.create_user(old_password)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.put(
+            self.url,
+            {'old_password': 'testuser_3', 'new_password': new_password},
+            format='json'
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_blank_new_password_can_change_password(self):
+        old_password = 'testuser_1'
+        new_password = ''
+        user, token = self.create_user(old_password)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.put(
+            self.url,
+            {'old_password': 'testuser_3', 'new_password': new_password},
+            format='json'
+        )
+        self.assertEqual(response.status_code, 400)
+
+    def test_no_new_password_can_change_password(self):
+        old_password = 'testuser_1'
+        user, token = self.create_user(old_password)
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        response = client.put(
+            self.url,
+            {'old_password': 'testuser_3', 'new_password': None},
+            format='json'
+        )
+        self.assertEqual(response.status_code, 400)
