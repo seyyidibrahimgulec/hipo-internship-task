860be56665ec0cd827304732117ea5e50bd2f3bc
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-06-20 16:38:48 +0300

    Fix mistakes

--- users/tests.py
+++ users/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from rest_framework.authtoken.models import Token
 from users.models import UserProfile
 from django.urls import reverse
+from rest_framework import status
 
 
 class UserRegistrationTestCase(TestCase):
@@ -15,7 +16,7 @@ class UserRegistrationTestCase(TestCase):
             {'username': 'testuser', 'email': 'testuser@mail.com', 'password': 'testuser_1'},
             format='json'
         )
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data.get('username'), 'testuser')
         self.assertEqual(response.data.get('email'), 'testuser@mail.com')
 
@@ -31,10 +32,10 @@ class UserRegistrationTestCase(TestCase):
             {'username': 'testuser', 'email': 'testuser2@mail.com', 'password': 'testuser_1'},
             format='json'
         )
-        self.assertEqual(response_user1.status_code, 201)
+        self.assertEqual(response_user1.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response_user1.data.get('username'), 'testuser')
         self.assertEqual(response_user1.data.get('email'), 'testuser1@mail.com')
-        self.assertEqual(response_user2.status_code, 400)
+        self.assertEqual(response_user2.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_existing_email_can_register(self):
         client = APIClient()
@@ -48,8 +49,8 @@ class UserRegistrationTestCase(TestCase):
             {'username': 'testuser2', 'email': 'testuser@mail.com', 'password': 'testuser_1'},
             format='json'
         )
-        self.assertEqual(response_user1.status_code, 201)
-        self.assertEqual(response_user2.status_code, 400)
+        self.assertEqual(response_user1.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response_user2.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_short_password_can_register(self):
         client = APIClient()
@@ -58,7 +59,7 @@ class UserRegistrationTestCase(TestCase):
             {'username': 'testuser', 'email': 'testuser@mail.com', 'password': 'test'},
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_entirely_numeric_password_can_register(self):
         client = APIClient()
@@ -67,7 +68,7 @@ class UserRegistrationTestCase(TestCase):
             {'username': 'testuser', 'email': 'testuser@mail.com', 'password': '12345678'},
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 class UserAuthenticationTestCase(TestCase):
@@ -95,36 +96,36 @@ class UserAuthenticationTestCase(TestCase):
         token = Token.objects.get(user=user)
         self.assertEqual(response.data.get('token'), token.key)
         self.assertEqual(response.data.get('username'), user.username)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_incorrect_email_can_authenticate(self):
         user, response = self.create_user_and_response('testuser@email.com', self.params['password'])
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_invalid_email_can_authenticate(self):
         user, response = self.create_user_and_response('testuseremailcom', self.params['password'])
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_blank_email_can_authenticate(self):
         user, response = self.create_user_and_response('', self.params['password'])
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_no_email_can_authenticate(self):
         user, response = self.create_user_and_response(email=None, password=self.params['password'])
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_incorrect_password_can_authenticate(self):
         password = self.params['password'] + '----'
         user, response = self.create_user_and_response(self.params['email'], password)
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_blank_password_can_authenticate(self):
         user, response = self.create_user_and_response(self.params['email'], '')
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_no_password_can_authenticate(self):
         user, response = self.create_user_and_response(email=self.params['email'], password=None)
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
 class MyProfileDetailTestCase(TestCase):
@@ -142,7 +143,7 @@ class MyProfileDetailTestCase(TestCase):
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = client.get(self.url, format='json')
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['username'], user.username)
         self.assertEqual(response.data['email'], user.email)
 
@@ -152,67 +153,63 @@ class MyProfileDetailTestCase(TestCase):
         client = APIClient()
         client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = client.get(self.url, format='json')
-        self.assertEqual(response.status_code, 401)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class ChangePasswordTestCase(TestCase):
     url = reverse('change_password')
 
-    def create_user(self, password):
+    def create_user_and_client(self, password):
         user = UserProfile.objects.create(username='testuser', email='testuser@mail.com')
         user.set_password(password)
         user.save()
         token, created = Token.objects.get_or_create(user=user)
-        return user, token
+        client = APIClient()
+        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        return user, client
 
     def test_user_can_change_password(self):
         old_password = 'testuser_1'
         new_password = 'testuser_2'
-        user, token = self.create_user(old_password)
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        user, client = self.create_user_and_client(old_password)
+        old_token = Token.objects.get(user=user)
         response = client.put(
             self.url,
             {'old_password': old_password, 'new_password': new_password},
             format='json'
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertNotEqual(response.data['token'], token.key)
+        new_token = Token.objects.get(user=user)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotEqual(old_token, new_token)
 
     def test_incorrect_password_can_change_password(self):
         old_password = 'testuser_1'
         new_password = 'testuser_2'
-        user, token = self.create_user(old_password)
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        user, client = self.create_user_and_client(old_password)
         response = client.put(
             self.url,
             {'old_password': 'testuser_3', 'new_password': new_password},
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_blank_new_password_can_change_password(self):
         old_password = 'testuser_1'
         new_password = ''
-        user, token = self.create_user(old_password)
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        user, client = self.create_user_and_client(old_password)
         response = client.put(
             self.url,
             {'old_password': 'testuser_3', 'new_password': new_password},
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_no_new_password_can_change_password(self):
         old_password = 'testuser_1'
-        user, token = self.create_user(old_password)
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+        user, client = self.create_user_and_client(old_password)
         response = client.put(
             self.url,
             {'old_password': 'testuser_3', 'new_password': None},
             format='json'
         )
-        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
