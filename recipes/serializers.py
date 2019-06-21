10f84a1943a85da029279debb1fe7aff7b9f5171
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-06-21 13:50:41 +0300

    Fix mistakes

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,20 +1,11 @@
 from rest_framework import serializers
 from recipes.models import Ingredient
 from drf_extra_fields.fields import Base64ImageField
-from django.db.utils import IntegrityError
-from django.utils.translation import ugettext_lazy
 
 
-class IngredientSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    name = serializers.CharField()
-    image = Base64ImageField(required=False)
+class IngredientSerializer(serializers.ModelSerializer):
+    image = Base64ImageField(required=True)
 
-    def create(self, validated_data):
-        try:
-            ingredient = Ingredient(name=validated_data['name'], image=validated_data['image'])
-            ingredient.save()
-            return ingredient
-        except IntegrityError:
-            msg = ugettext_lazy('The ingredient you have entered is already exist.')
-            raise serializers.ValidationError(msg)
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'image')
