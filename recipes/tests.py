cac9a3889225024ebfe1c93f341c58a754c7f9b9
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-06-24 12:27:22 +0300

    Divide create_ingredient method to two separate parts.

--- recipes/tests.py
+++ recipes/tests.py
@@ -11,7 +11,7 @@ class ListCreateIngredientTestCase(TestCase):
     url = reverse('list-create-ingredient')
     test_ingredient_name = 'test_ingredient'
 
-    def create_ingredient(self, name, fail_authenticate=False, username='testuser', email='testuser@mail.com'):
+    def create_user(self, fail_authenticate=False, username='testuser', email='testuser@mail.com'):
         user = UserProfile.objects.create_user(username=username, email=email, password='testuser_1')
         token, created = Token.objects.get_or_create(user=user)
         client = APIClient()
@@ -19,6 +19,9 @@ class ListCreateIngredientTestCase(TestCase):
             client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         else:
             client.credentials(HTTP_AUTHORIZATION='Token ' + token.key + 'fail')
+        return user, client
+
+    def create_ingredient(self, name, client):
         response = client.post(
             self.url,
             {'name': name, 'image': base64image},
@@ -33,22 +36,27 @@ class ListCreateIngredientTestCase(TestCase):
         return response
 
     def test_create_ingredient_with_user_authentication(self):
-        response = self.create_ingredient(self.test_ingredient_name)
+        user, client = self.create_user()
+        response = self.create_ingredient(self.test_ingredient_name, client)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data.get('name'), self.test_ingredient_name)
         self.assertIsNotNone(response.data.get('image'))
 
     def test_create_ingredient_without_user_authentication(self):
-        response = self.create_ingredient(self.test_ingredient_name, fail_authenticate=True)
+        user, client = self.create_user(fail_authenticate=True)
+        response = self.create_ingredient(self.test_ingredient_name, client)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_blank_ingredient(self):
-        response = self.create_ingredient('')
+        user, client = self.create_user()
+        response = self.create_ingredient('', client)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_same_name_ingredient(self):
-        response_1 = self.create_ingredient(self.test_ingredient_name)
-        response_2 = self.create_ingredient(self.test_ingredient_name, username='testuser2', email='testuser2@mail.com')
+        user_1, client_1 = self.create_user()
+        response_1 = self.create_ingredient(self.test_ingredient_name, client_1)
+        user_2, client_2 = self.create_user(username='testuser2', email='testuser2@mail.com')
+        response_2 = self.create_ingredient(self.test_ingredient_name, client_2)
         self.assertEqual(response_1.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response_1.data.get('name'), self.test_ingredient_name)
         self.assertEqual(response_2.status_code, status.HTTP_400_BAD_REQUEST)
