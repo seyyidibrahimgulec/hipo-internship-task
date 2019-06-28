2c45065bbf31aea01ae6db8874727dd3f1735050
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-06-28 12:40:18 +0300

    Delete redundant files and lines, Add documentation feature and Split urls

--- HipoProject/urls.py
+++ HipoProject/urls.py
@@ -16,39 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-# from recipes.views import (recipe_detail, index, NewRecipeView, like_recipe,
-#                            rate_recipe, search, UpdateRecipeView, ingredient,
-#                            DeleteRecipeView
-#                            )
-from django.urls import path
-from recipes.views import ListCreateIngredientView, ListCreateRecipeView, RecipeDetailView, \
-    ListCreateDeleteLikesView, CreateUpdateRatesView, CreateImageView
-# from django.contrib.auth.decorators import login_required
-from users.views import UserRegistrationView, UserAuthenticationView, MyProfileDetailView, ChangePasswordView
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/profiles/create/', UserRegistrationView.as_view(), name='create-user'),
-    path('api/profiles/authenticate/', UserAuthenticationView.as_view(), name='authenticate-user'),
-    path('api/profiles/me/', MyProfileDetailView.as_view(), name='my-profile-detail'),
-    path('api/profiles/me/change-password/', ChangePasswordView.as_view(), name='change-password'),
-    path('api/ingredients/', ListCreateIngredientView.as_view(), name='list-create-ingredient'),
-    path('api/recipes/', ListCreateRecipeView.as_view(), name='list-create-recipe'),
-    path('api/recipes/<int:pk>/', RecipeDetailView.as_view(), name='recipe-detail'),
-    path('api/recipes/<int:pk>/likes/', ListCreateDeleteLikesView.as_view(), name='like-recipe'),
-    path('api/recipes/<int:pk>/rates/', CreateUpdateRatesView.as_view(), name='rate-recipe'),
-    path('api/images/', CreateImageView.as_view(), name='create-image')
-    # path('', index, name="index"),
-    # path('recipe/<int:pk>/', recipe_detail, name='recipe_detail'),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # path('signup/', SignUp.as_view(), name='signup'),
-    # path('search/', search, name='search'),
-    # path('new_recipe/', login_required(NewRecipeView.as_view()), name='new_recipe'),
-    # path('update_recipe/<int:pk>/', login_required(UpdateRecipeView.as_view()), name='update_recipe'),
-    # path('delete_recipe/<int:pk>/', login_required(DeleteRecipeView.as_view()), name='delete_recipe'),
-    # path('like_recipe/<int:pk>/', like_recipe, name="like_recipe"),
-    # path('rate_recipe/<int:pk>/', rate_recipe, name="rate_recipe"),
-    # path('ingredient/<str:ingredient_value>/', ingredient, name='ingredient'),
+    path('', include('users.urls')),
+    path('', include('recipes.urls')),
+    path('docs/', include_docs_urls(title='Recipe API'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
