2f87e300fc4691e3742d03170edcda6e9fb2e279
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-01 16:11:13 +0300

    Change docs/ url to /

--- HipoProject/urls.py
+++ HipoProject/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls')),
     path('', include('recipes.urls')),
-    path('docs/', include_docs_urls(title='Recipe API'))
+    path('', include_docs_urls(title='Recipe API'))
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
