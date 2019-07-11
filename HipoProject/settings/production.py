e2c1de984ed34af9e4f6dbddd7f17a3d67ccb8ad
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-11 16:26:25 +0300

    Change production settings file

--- HipoProject/settings/production.py
+++ HipoProject/settings/production.py
@@ -3,6 +3,22 @@ import django_heroku
 
 DEBUG = False
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 ALLOWED_HOSTS = ['*']
 
 # Activate Django-Heroku.
