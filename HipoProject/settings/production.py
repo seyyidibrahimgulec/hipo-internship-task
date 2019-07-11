4373e8d27e1fa8bf94b79bcfab3c72f7f886f596
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-11 16:21:33 +0300

    Change production settings file

--- HipoProject/settings/production.py
+++ HipoProject/settings/production.py
@@ -1,20 +1,9 @@
 from .base import *
+import django_heroku
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-DISABLE_COLLECTSTATIC = 1
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db9dctg6lq3211',
-        'USER': 'ocveazvespixua',
-        'PASSWORD': 'cade7ca45e05c4498d8ae7466f79d1919db0efe987c0541c34f68a8cf315144b',
-        'HOST': 'ec2-174-129-29-101.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+# Activate Django-Heroku.
+django_heroku.settings(locals())
