e3a75da631b75d4f1df2a4fbd11bcc72b31d780b
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-11 17:30:06 +0300

    Update settings

--- HipoProject/settings/production.py
+++ HipoProject/settings/production.py
@@ -2,12 +2,12 @@ from .base import *
 import django_heroku
 
 DEBUG = False
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 ALLOWED_HOSTS = ['*']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
