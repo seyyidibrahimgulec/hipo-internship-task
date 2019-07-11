29d58e424999b10f8104ceb0add1dd6f79f67c83
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-11 16:39:34 +0300

    Change base settings

--- HipoProject/settings/base.py
+++ HipoProject/settings/base.py
@@ -102,15 +102,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 DEFAULT_CHARSET = 'utf-8'
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    'static/',
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 AUTH_USER_MODEL = 'users.UserProfile'
 
 REST_FRAMEWORK = {
