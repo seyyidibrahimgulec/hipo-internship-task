6f040daa3c4f13feaa173c5c6aca4378e320bfdf
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-12 15:27:22 +0300

    Change settings

--- settings/production.py
+++ settings/production.py
@@ -4,7 +4,7 @@ import django_heroku
 DEBUG = False
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-ALLOWED_HOSTS = ['tim-zed-31581.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
