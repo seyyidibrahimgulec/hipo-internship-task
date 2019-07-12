47d402252d96988ed1da8f30b6c34f5d1e76025a
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-12 17:40:01 +0300

    Update travis.yml and staging_test

--- settings/staging_test.py
+++ settings/staging_test.py
@@ -1,18 +1,11 @@
 from .base import *
 
-ALLOWED_HOSTS = ['*']
 
-DEBUG = True
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travis_ci_test',
+        'NAME': 'test_db',
         'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'PASSWORD': '',
     }
 }
