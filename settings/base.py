1db73c4d32f0a46a43a8664729ebcba6b5664faf
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-12 09:52:41 +0300

    Change settings

--- settings/base.py
+++ settings/base.py
@@ -112,6 +112,9 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(REPOSITORY_ROOT, 'media/')
+
 
 AUTH_USER_MODEL = 'users.UserProfile'
 
