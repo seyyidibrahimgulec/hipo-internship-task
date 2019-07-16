61ff26df66358a3c40cd052d9d0dbfec33e1dfc5
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-16 18:14:43 +0300

    Update .travis.yml and settings

--- settings/staging_test.py
+++ settings/staging_test.py
@@ -1,3 +1,15 @@
 from .base import *
 
 ALLOWED_HOSTS = ['*']
+
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'travisci',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
