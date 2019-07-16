ef747577792f3a4caaf6ae90a878a5621ed9b5e3
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-16 17:13:34 +0300

    Update settings and travis.yml

--- settings/staging_test.py
+++ settings/staging_test.py
@@ -1,11 +1,3 @@
 from .base import *
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['DATABASE_NAME'],
-        'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-    }
-}
+ALLOWED_HOSTS = ['*']
