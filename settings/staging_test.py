2e27a58624a9eef33e18387757329ca86b2dbf10
seyyidibrahimgulec <40296961+seyyidibrahimgulec@users.noreply.github.com>
2019-07-16 16:39:33 +0300

    Update staging settings file

--- settings/staging_test.py
+++ settings/staging_test.py
@@ -6,7 +6,5 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DATABASE_NAME'],
         'USER': os.environ['DATABASE_USER'],
-        'PASSWORD': os.environ['DATABASE_PASSWORD'],
-        'HOST': 'localhost',
     }
 }
